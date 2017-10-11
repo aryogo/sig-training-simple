c107e6ad55f63199f51168f64204b199c3f3c253
IT060717 <IT060717@NBC0JJ558ITSD.corp.bankbtpn.co.id>
2017-10-11 15:39:45 +0700

    simplecode

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -1,6 +1,7 @@
 package eu.sig.training.ch04;
+import eu.sig.training.ch04.counterAccount;
 
-public class CheckingAccount {
+public class CheckingAccount extends counterAccount{
     private static final float INTEREST_PERCENTAGE = 0.01f;
     private Money balance = new Money();
     private int transferLimit = 100;
@@ -12,20 +13,7 @@ public class CheckingAccount {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
-        } else {
-            throw new BusinessException("Invalid account number!");
-        }
+        super.testing();
     }
 
     public void addInterest() {
